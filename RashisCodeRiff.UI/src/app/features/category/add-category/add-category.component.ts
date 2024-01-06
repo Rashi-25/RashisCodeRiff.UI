1c1954e204e3869beedd942ad5e526d34b4eae2d
Rashi-25 <it.rashi25@gmail.com>
2024-01-06 21:18:21 +0530

    Blog UI Part

---
+++ RashisCodeRiff.UI/src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css']
+})
+export class AddCategoryComponent implements OnDestroy{
+
+  model: AddCategoryRequest;
+  private addCategorySubscribtion?: Subscription;
+
+  constructor(private categoryService: CategoryService){
+    this.model = {
+      Name: '',
+      UrlHandle: ''
+    };
+  }
+  
+
+  onFormSubmit(){
+    this.addCategorySubscribtion = this.categoryService.addCategory(this.model)
+    .subscribe({
+      next: (response) => {
+        console.log("Succcessssssssssssssssss!");
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    this.addCategorySubscribtion?.unsubscribe();
+  }
+
+}
