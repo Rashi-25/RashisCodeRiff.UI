668aec677e3c60d0359bed14c8c30d1abb355113
Rashi-25 <it.rashi25@gmail.com>
2024-01-13 19:58:26 +0530

    Add Blog Post UI Component

---
+++ RashisCodeRiff.UI/src/app/features/blog-post/services/blog-post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BlogPost } from '../models/blog-post.model';
+import { Observable } from 'rxjs';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  constructor(private http: HttpClient) { }
+  
+  createBlogPost(data: AddBlogPost) : Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment.baseApiUrl}/api/blogposts?addAuth=true`, data);
+  }
+}
